dba284e1042f6e611d4ff4323d88d98b9ba643ad
Mandaliya Akshar 😀😀 <mandaliyaakshar99@gmail.com>
2024-12-16 16:06:07 +0530

    Add files via upload

---
+++ Demo.py
@@ -0,0 +1,13 @@
+import numpy as np
+array1 = np.array([10, 20, 30, 40, 50])
+array2 = np.array([5, 15, 25, 35, 45])
+addition = array1 + array2
+subtraction = array1 - array2
+multiplication = array1 * array2
+division = array1 / array2
+print("Array 1:", array1)
+print("Array 2:", array2)
+print("Addition:", addition)
+print("Subtraction:", subtraction)
+print("Multiplication:", multiplication)
+print("Division:", division)
